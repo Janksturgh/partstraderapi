49ef0234dc7fe600e94c494cb408971ad7cece81
Janksturgh <jai.barlow@hotmail.com>
2025-06-01 00:25:31 +1200

    adding description field to banner and layout

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,14 +3,16 @@ import './banner.scss';
 /**
  * @description Banner component rendering a title to be displayed on the page.
  * @param {string} title - The title to be displayed in the banner.
+ * @param {string} description - An optional description to be displayed below the title.
  * @returns {JSX.Element} A JSX element representing the banner with the title.
  */
 
-export default function Banner({ title }) {
+export default function Banner({ title, description = '' }) {
   return (
     <div className="banner">
       <div className="container-standard">
         <h1 className="banner__title">{title}</h1>
+        <p>{description}</p>
       </div>
     </div>
   );
--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -4,14 +4,15 @@ import Header from '@/components/Header';
 /**
  * @description Layout component serves as a wrapper for the main content of the application.
  * @param {string} title - The title to be displayed in the main section.
+ * @param {string} description - A brief description to be displayed in the main section.
  * @param {JSX.Element} children - The child components to be rendered within the main section.
  * @returns {JSX.Element} - A JSX element representing the layout with a header and main content area.
  */
-export default function Layout({ title, children }) {
+export default function Layout({ title, description = '', children }) {
   return (
     <>
       <Header />
-      <Banner title={title} />
+      <Banner title={title} description={description} />
       <main role="main" className="container-standard">
         {children}
       </main>
