bf3d29b7470cc70e9b0009d3f8e77fa096f39756
Janksturgh <jai.barlow@hotmail.com>
2025-06-01 00:25:41 +1200

    starting form on part lookup page

--- src/views/PartLookupPage.jsx
+++ src/views/PartLookupPage.jsx
@@ -1,5 +1,22 @@
 import Layout from '@/layouts/Layout.jsx';
 
 export default function PartLookupPage() {
-  return <Layout title="Part Lookup"></Layout>;
+  return (
+    <Layout title="Part Lookup" description="Search for a specific part by its ID">
+      <div className="part-lookup-page">
+        <form className="part-lookup-page__form">
+          <label htmlFor="part-id">Part ID:</label>
+          <input
+            className="part-lookup-page__input"
+            type="text"
+            id="part-id"
+            name="part-id"
+            placeholder="Enter part ID"
+            required
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+    </Layout>
+  );
 }
