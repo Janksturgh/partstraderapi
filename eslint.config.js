cd9acdd232ecc4655d0de1c4737330a4f19e13e9
Janksturgh <jai.barlow@hotmail.com>
2025-05-31 23:36:31 +1200

    removing prettier config files in place of the eslint prettier plugin and adding that config to eslint config, also adding lint fix script and removing prettier format scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
   { ignores: ['dist'] },
@@ -19,15 +21,25 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'prettier/prettier': [
+        'error',
+        {
+          semi: true,
+          singleQuote: true,
+          tabWidth: 2,
+          printWidth: 100,
+          trailingComma: 'es5',
+        },
       ],
     },
   },
-]
+  // This disables conflicting formatting rules from other plugins
+  eslintConfigPrettier,
+];
