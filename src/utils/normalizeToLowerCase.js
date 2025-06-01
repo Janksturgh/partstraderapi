ddcd331967927e8f546bc14fc816fc50fb9a17e2
Janksturgh <jai.barlow@hotmail.com>
2025-06-01 18:45:58 +1200

    creating normalize lower case function and test to ensure strings handed in with uppercase words get lowercased

---
+++ src/utils/normalizeToLowerCase.js
@@ -0,0 +1,10 @@
+/**
+ * @description the normalizeToLowerCase function normalizes a string to lowercase which can be used
+ * to ensure consistent case handling in string comparisons.
+ * @param {string} string - The string to normalize.
+ * @returns {string} The normalized string in lowercase.
+ */
+
+export default function normalizeToLowerCase(string) {
+  return string.toLowerCase();
+}
