305ce85f5e63822fa7a7794cefd021449380cdb0
Janksturgh <jai.barlow@hotmail.com>
2025-06-15 21:32:59 +1200

    deleting utils as the backend api will handle them now

--- src/utils/normalizeToLowerCase.test.js
+++
@@ -1,8 +0,0 @@
-import normalizeToLowerCase from './normalizeToLowerCase';
-import { describe, it, expect } from 'vitest';
-
-describe('normailzeCase', () => {
-  it('should convert the string to lowercase', () => {
-    expect(normalizeToLowerCase('1234-ABCD')).toEqual('1234-abcd');
-  });
-});
